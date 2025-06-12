3ec4356a0699fb7e8bfe0398671dba759b654ea0
Aulia Intan Shafira <aulins12.ais@gmail.com>
2025-06-12 19:28:32 +0700

    perbaiki perutean

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,10 @@
-</div> <!-- end container -->
-<footer class="bg-light text-center p-3 mt-5">
-    <small>&copy; <?= date('Y') ?> Jasa Website Lengkap & Profesional</small>
+<footer class="footer fixed-bottom bg-light p-3 mt-5">
+    <div class="container text-center">
+        <span class="text-white">
+            This is the sticky footer.
+        </span>
+        <small>&copy; <?= date('Y') ?> Jasa Website Lengkap & Profesional</small>
+    </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,8 +7,27 @@
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="<?= base_url() ?>">Jasa Website</a>
-    </div>
+        <div class="container">
+            <a class="navbar-brand" href="<?= base_url() ?>">Jasa Website Profesional</a>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?= base_url() ?>">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('user/katalog') ?>">Katalog</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('order/create') ?>">Order</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('review/create') ?>">Ulasan</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('contact') ?>">Kontak</a>
+            </li>
+        </ul>
+        </div>
+        </div>
     </nav>
 <div class="container mt-4">
